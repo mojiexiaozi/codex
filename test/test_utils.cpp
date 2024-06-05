d3064c4314e0cb43e04608812f4eb5f8740dcea0
ylin <ylin3759@gmai.com>
2024-06-05 20:18:34 +0800

    add test

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -5,5 +5,25 @@ TEST(UtilsTest, VectorFind)
 {
     std::vector<int> int_vec{1,2,3,4};
     EXPECT_EQ(utils::find(int_vec, 1), 0);
+    EXPECT_EQ(utils::find(int_vec, 2), 1);
+    EXPECT_EQ(utils::find(int_vec, 3), 2);
+    EXPECT_EQ(utils::find(int_vec, 4), 3);
     EXPECT_EQ(utils::find(int_vec, 5), -1);
+
+    std::vector<float> float_vec{1.1,2.1,3.1,4.1};
+    EXPECT_EQ(utils::find(float_vec, 1.1), 0);
+    EXPECT_EQ(utils::find(float_vec, 0.1), -1);
+
+    std::vector<std::string> str_vec{"1.1","2.1","3.1","4.1"};
+    EXPECT_EQ(utils::find(float_vec, "1.1"), 0);
+    EXPECT_EQ(utils::find(float_vec, "0.1"), -1);
+}
+
+TEST(UtilsTest, VectorFindIf)
+{
+    std::vector<int> int_vec{1,2,3,4};
+    EXPECT_EQ(utils::find(int_vec, [](int n){return n%2==0;}), 1);
+    EXPECT_EQ(utils::find(int_vec, [](int n){return n%3==0;}), 2);
+    EXPECT_EQ(utils::find(int_vec, [](int n){return n%5==0;}), -1);
+
 }
